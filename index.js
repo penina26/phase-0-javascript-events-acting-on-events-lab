41a71845576bb6cf9ba09c15a012309c926dbcec
Penina <wpenina@gmail.com>
2025-08-25 19:00:54 +0300

    move elements

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const currentLeft = parseInt(dodger.style.left) || 0;
+
+    // Decrease the 'left' value to move the element to the left
+    dodger.style.left = (currentLeft - 15) + 'px';
+}
+
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const currentLeft = parseInt(dodger.style.left) || 0;
+
+    // Increase the 'left' value to move the element to the right
+    dodger.style.left = (currentLeft + 15) + 'px';
+}
